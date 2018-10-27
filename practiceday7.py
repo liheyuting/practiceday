d3a4a33c1628b7605573e1515485d307bbd24124
liheyuting <liheyuting@163.com>
2018-10-27 18:47:13 +0800

    practiceday8

---
+++ practiceday7.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/27 6:08 PM
+# @Author  : Dora
+# @File    : practiceday7.py
+
+
+#题目：将一个列表的数据复制到另一个列表中。
+l = [1,2,3,4,5,6]
+l2 = l[0:]
+print(l2)
+
+
+#参考答案
+a = [1, 2, 3]
+b = a[:]
+print(b)
---
+++ practiceday9.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/27 6:46 PM
+# @Author  : Dora
+# @File    : practiceday9.py
