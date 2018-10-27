67d074da2116bbe96c46149c16956555871e587c
liheyuting <liheyuting@163.com>
2018-10-27 22:36:45 +0800

    practiceday9

---
+++ practiceday10.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/27 10:34 PM
+# @Author  : Dora
+# @File    : practiceday10.py
--- practiceday9.py
+++ practiceday9.py
@@ -3,3 +3,14 @@
 # @Time    : 2018/10/27 6:46 PM
 # @Author  : Dora
 # @File    : practiceday9.py
+
+
+#题目：暂停一秒输出
+import time
+
+x = 9
+def timedelay(i):
+    i = x+1
+    time.sleep(7)
+
+print(timedelay(x))
